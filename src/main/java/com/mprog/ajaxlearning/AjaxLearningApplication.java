a000461f55e013fa0f6c471574ca1ade6c8ea368
Munir Zikrullaev <munirprog@gmail.com>
2021-10-10 15:54:16 +0300

    init commit and add some properties

---
+++ src/main/java/com/mprog/ajaxlearning/AjaxLearningApplication.java
@@ -0,0 +1,13 @@
+package com.mprog.ajaxlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjaxLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mprog/ajaxlearning/AjaxLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mprog.ajaxlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
